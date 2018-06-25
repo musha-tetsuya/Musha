da444c4a2541ccd073d9bc1cd06c77af98eb53b0
musha-tetsuya <the_beatlesan@hotmail.co.jp>
2018-06-25 21:45:04 +0900

    EnumFlagsAttribute作成

---
+++ Attribute/EnumFlagsAttribute.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public sealed class EnumFlagsAttribute : PropertyAttribute
+{
+	public Type enumType = null;
+
+	public EnumFlagsAttribute(Type _enumType = null)
+	{
+		this.enumType = _enumType;
+	}
+}
+
+#if UNITY_EDITOR
+[CustomPropertyDrawer(typeof(EnumFlagsAttribute))]
+public sealed class EnumFlagsAttributeDrawer : PropertyDrawer
+{
+	public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+	{
+		var enumType = (this.attribute as EnumFlagsAttribute).enumType;
+
+		string[] displayedOptions = (enumType == null) ? property.enumNames : Enum.GetNames(enumType);
+
+		property.intValue = EditorGUI.MaskField(position, label, property.intValue, displayedOptions);
+	}
+}
+#endif
