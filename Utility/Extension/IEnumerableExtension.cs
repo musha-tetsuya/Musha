7662732fee7eb4d88730a8e0e3409f4e3cac7f0a
musha-tetsuya <the_beatlesan@hotmail.co.jp>
2018-09-06 21:37:51 +0900

    IEnumerable拡張メソッド作成

---
+++ Utility/Extension/IEnumerableExtension.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Musha {
+
+/// <summary>
+/// IEnumerable拡張
+/// </summary>
+public static class IEnumerableExtension
+{
+	/// <summary>
+	/// 全要素に処理を行う
+	/// </summary>
+	public static void ForEach<T>(this IEnumerable<T> source, Action<T> action)
+	{
+		foreach (T element in source)
+		{
+			action(element);
+		}
+	}
+}
+
+}//namespace Musha
